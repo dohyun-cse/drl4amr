7763b1f0ddb86963b502450f189d850ef43b3199
dohyun <dohyun_kim@brown.edu>
2023-04-13 11:48:42 -0400

    initialization

--- src/envs.py
+++ src/envs.py
@@ -9,7 +9,7 @@ from gymnasium import spaces, utils
 from gymnasium.spaces import Box, Discrete
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 import logging
-from typing import Callable, Dict, List, Tuple, Optional, Union, Set, Type
+from typing import Callable, Dict, List, Tuple, Optional, Union, Set, Type, Sequence
 
 from ray.rllib.env.base_env import BaseEnv
 from ray.rllib.env.env_context import EnvContext
@@ -38,33 +38,87 @@ else:
 
 class HyperbolicAMREnv(MultiAgentEnv):
     def __init__(self, 
-                 solver_name:str, 
-                 solver_args:Dict=None,
-                 regrid_time=-1.0,
-                 refine_mode='p',
-                 window_size=3, 
-                 seed:Optional[float]=None):
+                 solver_name:str,
+                 num_grids:Sequence[int]=[10, 10],
+                 domain_size:Sequence[float]=[1., 1.],
+                 offsets:Sequence[float]=[0., 0.],
+                 regrid_time:float=-1.0,
+                 terminal_time:float= -1.0,
+                 refine_mode:str='p',
+                 window_size:int=3,
+                 visualization:bool=False,
+                 seed:Optional[int]=None,
+                 solver_args:Dict=None):
+        
+        #region Create Mesh
+        # Uniform tensor elements
+        self.num_grids = num_grids
+        if len(num_grids) == 1:
+            self.mesh:mfem.Mesh = mfem.Mesh.MakeCartesian1D(n=num_grids[0], sx=domain_size[0])
+            translations = (mfem.Vector([domain_size[0]]))
+        elif len(num_grids) == 2:
+            self.mesh:mfem.Mesh = mfem.Mesh.MakeCartesian2D(nx=num_grids[0], ny=num_grids[1], sx=domain_size[0], sy=domain_size[1], sfc_ordering=False, type=mfem.Element.QUADRILATERAL)
+            translations = (mfem.Vector([domain_size[0], 0.]), mfem.Vector([0., domain_size[1]]))
+        elif len(num_grids) == 3:
+            self.mesh:mfem.Mesh = mfem.Mesh.MakeCartesian3D(nx=num_grids[0], ny=num_grids[1], nz=num_grids[2], sx=domain_size[0], sy=domain_size[1], sz=domain_size[2], sfc_ordering=False, type=mfem.Element.HEXAHEDRON)
+            translations = (mfem.Vector([domain_size[0], 0., 0.]), mfem.Vector([0., domain_size[1], 0.]), mfem.Vector([0., 0., domain_size[2]]))
+            
+        # periodic boundary
+        self.mesh.CreatePeriodicVertexMapping(translations)
+        
+        # Shift domain if necessary
+        if not all(np.equal(offsets, 0)):
+            if len(offsets) == 1:
+                @mfem.jit.vector(vdim=1, interface="c++", sdim=1, dependency=(offsets))
+                def Velocity(x, out):
+                    out[0] = x[0] - offsets[0]
+            elif len(offsets) == 2:
+                @mfem.jit.vector(vdim=2, interface="c++", sdim=2, dependency=(offsets))
+                def Velocity(x, out):
+                    out[0] = x[0] - offsets[0]
+                    out[1] = x[1] - offsets[1]
+            elif len(offsets) == 3:
+                @mfem.jit.vector(vdim=3, interface="c++", sdim=3, dependency=(offsets))
+                def Velocity(x, out):
+                    out[0] = x[0] - offsets[0]
+                    out[1] = x[1] - offsets[1]
+                    out[2] = x[2] - offsets[2]
+            self.mesh.Transform(Velocity)
+            
+        # Make it non-conforming mesh
+        self.mesh.EnsureNCMesh()
+        
+        # build mapping between elements
+        self.obs_map = self.build_obs_map()
+        
+        #endregion
+        
+        
+        #region Create Solver
         if solver_name == 'advection':
-            self.solver = solver.AdvectionSolver(**solver_args)
+            self.solver = solver.AdvectionSolver(mesh=self.mesh, **solver_args)
         elif solver_name == 'burgers':
-            self.solver = solver.BurgersSolver(**solver_args)
+            self.solver = solver.BurgersSolver(mesh=self.mesh, **solver_args)
         elif solver_name == 'euler':
-            self.solver = solver.EulerSolver(**solver_args)
+            self.solver = solver.EulerSolver(mesh=self.mesh, **solver_args)
         else:
             raise ValueError(f'Unsupported solver name: {solver_name}.')
+        #endregion
         
         if regrid_time < 0 or regrid_time > solver_args.get('terminal_time', 2):
             raise ValueError(f'Regrid time is not provided or larger than the terminal time: {regrid_time}')
         
+        self.observation_norm = 'L2'
+        self.flux_observation = 'at_regrid_time'
         self.regrid_time = regrid_time
+        self.terminal_time = terminal_time
         self.refine_mode = refine_mode
         self.window_size = window_size
-        self.randomize = seed is not None
         self.seed = seed
-    
+        self.visualization = visualization
+        
     def reset(self, *, seed: Optional[int]=None, options: Optional[dict] = None):
         self.solver.reset()
-        
     
     def step(self, action_dict:MultiAgentDict) -> Tuple[
         MultiAgentDict, MultiAgentDict, MultiAgentDict, MultiAgentDict, MultiAgentDict
@@ -149,4 +203,28 @@ class HyperbolicAMREnv(MultiAgentEnv):
     def render(self):
         self.solver.render()
     
-    
+    def build_obs_map(self):
+        """obs[:,i] = element indices within the window size. Only uniform periodic rectangular mesh is supported.
+        """
+        sdim = self.solver.sdim
+        obs_map = np.zeros(((self.window_size*2 + 1)**self.solver.sdim, self.solver.mesh()), dtype=int)
+        idx = np.arange(np.prod(self.num_grids)).reshape(self.num_grids)
+        if sdim == 1:
+            for x_offset in range(self.window_size*2 + 1):
+                obs_map[x_offset, :] = np.roll(idx, (-self.window_size + x_offset))
+        elif sdim == 2:
+            i = 0
+            for y_offset in range(self.window_size*2 + 1):
+                for x_offset in range(self.window_size*2 + 1):
+                    i += 1
+                    obs_map[i] = np.roll(idx, (-self.window_size + x_offset, -self.window_size + y_offset), axis=(0,1))
+        elif sdim == 3:
+            i = 0
+            for z_offset in range(self.window_size*2 + 1):
+                for y_offset in range(self.window_size*2 + 1):
+                    for x_offset in range(self.window_size*2 + 1):
+                        i += 1
+                        obs_map[i] = np.roll(idx, (-self.window_size + x_offset, -self.window_size + y_offset, -self.window_size + z_offset), axis=(0,1,2))
+        
+        
+        
